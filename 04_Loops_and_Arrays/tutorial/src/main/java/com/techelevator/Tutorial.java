c835c0645ad6eed7cceed0558ab0874247ce9116
Ricardo Ramos <ricardo.r.p466@outlook.com>
2022-04-21 21:06:40 -0400

    Tutorial finished

--- 04_Loops_and_Arrays/tutorial/src/main/java/com/techelevator/Tutorial.java
+++ 04_Loops_and_Arrays/tutorial/src/main/java/com/techelevator/Tutorial.java
@@ -3,9 +3,30 @@ package com.techelevator;
 public class Tutorial {
 
     public static void main(String[] args) {
+        int[] forecastTemperatures = new int [5];
+        forecastTemperatures[0] = 72;
+        forecastTemperatures[1] = 78;
+        forecastTemperatures[2] = 58;
+        forecastTemperatures[3] = 79;
+        forecastTemperatures[4] = 74;
 
+        int highestTemperatureValue = forecastTemperatures[0];
+        int highestTemperatureIndex = 0;
         // write your code here
+        int i;
+        for (i=0; i<forecastTemperatures.length; i++) {
+            System.out.println(forecastTemperatures[i]);
+        }
+        for (int j = 1; j < forecastTemperatures.length; j++){
+            if (forecastTemperatures[j] > highestTemperatureValue){
+                highestTemperatureValue = forecastTemperatures[j];
+                highestTemperatureIndex = j;
 
+            }
+
+        }
+        System.out.println("The highest temperature is " + highestTemperatureValue);
+        System.out.println("The highest temperature is in " +(highestTemperatureIndex + 1) + " days");
 
 
 
