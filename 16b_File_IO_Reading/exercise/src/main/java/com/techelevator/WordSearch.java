9d7551d8917ab34633eac88f572430c96c545441
Ricardo Ramos <ricardo.r.p466@gmail.com>
2022-05-25 00:07:58 -0400

    exercises done apart from challenge

--- 16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
+++ 16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
@@ -1,8 +1,38 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class WordSearch {
 
 	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
+		int lineCounter = 0;
+		System.out.println("Enter the file path name: ");
+		//String[] input = userInput.nextLine().split(" ");
+		String file = userInput.nextLine();
+		System.out.println("Enter word to search: ");
+		String wordToFind = userInput.nextLine();
+		System.out.println("should search be case sensitive? "+ "(Y/N)");
+		String caseSensitive = userInput.nextLine().toUpperCase();
+		File fileInput = new File(file);
+		try(Scanner BookFile = new Scanner(fileInput)) {
+			while (BookFile.hasNextLine()){
+				String lineOfText = BookFile.nextLine();
+				lineCounter++;
+				if (lineOfText.contains(wordToFind) && caseSensitive.contains("Y")){
+					System.out.println(lineCounter + ": " + lineOfText);
+				}
+				if (lineOfText.toLowerCase().contains(wordToFind.toLowerCase()) && caseSensitive.contains("N")){
+					System.out.println(lineCounter + ": " + lineOfText);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getStackTrace());
+		}
+
 
 	}
 
--- 16b_File_IO_Reading/tutorial/src/main/java/com/techelevator/BookReader.java
+++ 16b_File_IO_Reading/tutorial/src/main/java/com/techelevator/BookReader.java
@@ -22,11 +22,53 @@ public class BookReader {
         Scanner userInput = new Scanner(System.in);
         // Prompt the user for a file path - path should look like "data/jekyll-and-hyde.txt"
         System.out.print("Enter path to the book file: ");
-        String filePath = userInput.nextLine();
+        String filePath = "tutorial/data/" + userInput.nextLine().replace(" ","-").toLowerCase() +".txt";
 
         /*
         Step 2: Step Two: Open the book file and handle errors
          */
+        // Create a File object using the path
+        File bookFile = new File(filePath);
+        int lineCount = 0;
+        int blankLineCounter = 0;
+        //Open the file
+        try (Scanner fileInput = new Scanner(bookFile)){
+            /*
+            Step 3: Create a read loop and process the lines
+             */
+            boolean inBookText = false; // Are you reading between the start and end markers?
+            // loop until the end of file is reached
+            while (fileInput.hasNextLine()){
+                // Read the next line into 'lineOfText'
+                String lineOfText = fileInput.nextLine();
+                 /*
+                Step 4: Skip the header information before book content
+                 */
+                if (lineOfText.startsWith(BEGIN_MARKER)){
+                    inBookText = true;
+                    continue; // no need to process this...go to the next
+                }
+                if (lineOfText.startsWith(END_MARKER)){
+                    break;
+                }
+                if (inBookText) {
+                    // Increment the line count.
+                    lineCount++;
+                    //Print the line
+                    if(lineOfText.length()==0 && blankLineCounter==0){
+                        blankLineCounter++;
+                        System.out.println(lineCount + ": " + lineOfText);
+                    }
+                     if(lineOfText.length()!=0){
+                         blankLineCounter = 0;
+                         System.out.println(lineCount + ": " + lineOfText);
+                     }
+                }
 
+            }
+        } catch (FileNotFoundException e){
+            System.out.println("The file was not found" + bookFile.getAbsolutePath());
+        }
+        System.out.println("Found " + lineCount + " lines of text in " + filePath);
     }
 }
